71095aed1b2ab78abb6aeb162db9f35fb803d007
Ajay Upadhyay <ajayupadhyay030@gmail.com>
2026-01-11 23:05:38 +0530

    nav item removed

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -216,18 +216,6 @@
                             </Link>
                         </li>
                         <li>
-                            <a
-                                href="#services"
-                                class="group flex items-center gap-4 px-4 py-4 text-lg font-semibold text-white hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-500 rounded-xl transition-all duration-300"
-                                @click="closeMenu"
-                            >
-                                <svg class="w-5 h-5 text-orange-500 group-hover:text-white transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
-                                </svg>
-                                <span>Services</span>
-                            </a>
-                        </li>
-                        <li>
                             <Link
                                 href="/blog"
                                 class="group flex items-center gap-4 px-4 py-4 text-lg font-semibold text-white hover:bg-gradient-to-r hover:from-orange-500 hover:to-red-500 rounded-xl transition-all duration-300"
